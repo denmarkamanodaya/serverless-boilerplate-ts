716e46d66cfb0ac63e23e49068b9c3b4f2fd845e
Denmark Daya <denmarkamanodaya@gmail.com>
2026-02-04 19:53:28 +0800

    delete unwanted files

--- .build/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.withMiddleware = void 0;
-const chain_1 = require("./chain");
-const withMiddleware = (handler, extra = []) => {
-    const chain = [...chain_1.defaultMiddlewares, ...extra];
-    return chain.reduceRight((next, middleware) => middleware(next), handler);
-};
-exports.withMiddleware = withMiddleware;

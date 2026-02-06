ba8cf00a41edd17ef3566c9a808e68c1706be292
Denmark Daya <denmarkamanodaya@gmail.com>
2026-02-06 21:54:35 +0800

    Added initial 100% code coverage

--- tests/fixtures/test.fixtures.ts
+++ tests/fixtures/test.fixtures.ts
@@ -1,7 +1,20 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 
-export const validTestRequestFixtures: APIGatewayProxyEvent = {
-  body: JSON.stringify({ title: 'TEST TITLE', completed: true }),
+// export const validTestRequest: APIGatewayProxyEvent = {
+//   ...(body = { title: 'TEST TITLE', completed: true }),
+// };
+
+export const validResponse = 'Hello from Serverless + TypeScript + Offline!';
+export const objectPayload = { foo: 'bar' };
+export const validPayload = { title: 'TEST TITLE', completed: true };
+export const invalidPayload = '{invalid JSON}';
+
+export const makeEvent = (body: any): APIGatewayProxyEvent => {
+  return { ...baseEvent, body: body as any };
+};
+
+export const baseEvent: APIGatewayProxyEvent = {
+  body: '',
   headers: {},
   multiValueHeaders: {},
   httpMethod: 'POST',
--- tests/unit/handlers/test/v1.test.ts
+++ tests/unit/handlers/test/v1.test.ts
@@ -1,18 +1,38 @@
 import { handler } from '../../../../src/handlers/test/v1';
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { Context } from 'aws-lambda';
 import httpStatus from 'http-status';
-import { validTestRequestFixtures } from '../../../fixtures/test.fixtures';
+import { makeEvent, validPayload, objectPayload, invalidPayload, validResponse } from '../../../fixtures/test.fixtures';
 
 describe('Lambda handler', () => {
   const context: Context = {} as Context;
 
   it('should return OK with parsed body', async () => {
-    const result = await handler(validTestRequestFixtures, context);
+    const request = makeEvent(JSON.stringify(validPayload));
+    const result = await handler(request, context);
 
     expect(result.statusCode).toBe(httpStatus.OK);
     expect(JSON.parse(result.body)).toEqual({
-      message: 'Hello from Serverless + TypeScript + Offline!',
-      input: { title: 'TEST TITLE', completed: true }, // <- parsed body
+      input: validPayload,
+      message: validResponse,
     });
+  });
+
+  it('should pass body as-is if body is already an object', async () => {
+    const request = makeEvent(objectPayload);
+    const result = await handler(request, context);
+
+    expect(result.statusCode).toBe(httpStatus.OK);
+    expect(JSON.parse(result.body)).toEqual({
+      input: objectPayload,
+      message: validResponse,
+    });
+  });
+
+  it('should handle catch block', async () => {
+    const request = makeEvent(invalidPayload);
+    const result = await handler(request, context);
+
+    expect(result.statusCode).toBe(httpStatus.BAD_REQUEST);
+    expect(JSON.parse(result.body)).toEqual({ message: 'Invalid JSON body' });
   });
 });

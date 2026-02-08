18ed8c05d3114d137f1aaaabed7d881df947e844
Denmark Daya <denmarkamanodaya@gmail.com>
2026-02-08 10:38:34 +0800

    Refactor: Middleware clean-up removed, json.middleware added into the main middleware instead

--- src/middlewares/chain.ts
+++ src/middlewares/chain.ts
@@ -1,5 +1,4 @@
 import { Middleware } from './middleware';
-import { jsonBodyMiddleware } from './body-parser.middleware';
 import { responseMiddleware } from './response.middlware';
 
-export const defaultMiddlewares: Middleware[] = [jsonBodyMiddleware, responseMiddleware];
+export const defaultMiddlewares: Middleware[] = [responseMiddleware];

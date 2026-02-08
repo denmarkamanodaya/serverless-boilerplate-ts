fa304d75fb7e2792b981cd9b23e5012e360ca76c
Denmark Amano Daya <denmarkamanodaya@gmail.com>
2026-02-08 10:39:01 +0800

    Refactor: Middleware clean-up removed, json.middleware added into the main middleware instead (#10)

--- tests/unit/middlewares/chain.test.ts
+++ tests/unit/middlewares/chain.test.ts
@@ -1,12 +1,10 @@
 import { defaultMiddlewares } from '../../../src/middlewares/chain';
-import { jsonBodyMiddleware } from '../../../src/middlewares/body-parser.middleware';
 import { responseMiddleware } from '../../../src/middlewares/response.middlware';
 
 describe('defaultMiddlewares', () => {
   it('exports the correct middlewares array', () => {
-    expect(defaultMiddlewares).toEqual([jsonBodyMiddleware, responseMiddleware]);
-    expect(defaultMiddlewares.length).toBe(2);
-    expect(defaultMiddlewares[0]).toBe(jsonBodyMiddleware);
-    expect(defaultMiddlewares[1]).toBe(responseMiddleware);
+    expect(defaultMiddlewares).toEqual([responseMiddleware]);
+    expect(defaultMiddlewares.length).toBe(1);
+    expect(defaultMiddlewares[0]).toBe(responseMiddleware);
   });
 });
